ccb60e5be298e6d434b3ff59919db6b6eacac25e
Gabriel Araújo <araujooj@MacBook-Air-de-Gabriel.local>
2021-08-20 15:47:59 -0300

    alteraçao

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload - Entrega.
+          Edit <code>src/Aapp</code> and save to reload - Entrega.
         </p>
         <a
           className="App-link"
